b410e6197bea1eb88c5569173b1f7670f96afed8
giahung123456 <dogiahungtplc@gmail.com>
2026-05-21 11:48:03 +0700

    fixx

---
+++ src/main/java/com/example/ss5_02/Ss502Application.java
@@ -0,0 +1,13 @@
+package com.example.ss5_02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ss502Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ss502Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ss5_02/Ss502ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ss5_02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ss502ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
